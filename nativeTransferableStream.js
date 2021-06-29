c05c6a7d46e8e2db253a551780cfb5a8a92778a5
guest271314 <guest271314@gmail.com>
2021-06-28 17:31:47 -0700

    Update nativeTransferableStream.js

--- nativeTransferableStream.js
+++ nativeTransferableStream.js
@@ -1,24 +1,30 @@
-async function nativeTransferableStream(readable) {
-  return new Promise(async (resolve) => {
-    onmessage = async (e) => {
-      if (e.data === 'Ready') {
-        e.source.postMessage(readable, e.origin, [readable]);
-      } else {
-        console.log(e.data);
-      }
-      if (e.data instanceof ReadableStream) {
-        const message = await audioStream(e.data);
-        onmessage = null;
-        resolve(message);
-      }
-    };
-
-    const transferableWindow = document.createElement('iframe');
-    transferableWindow.style.display = 'none';
-    transferableWindow.name = location.href;
-    transferableWindow.src = 'chrome-extension://<id>/nativeTransferableStream.html';
-    document.body.appendChild(transferableWindow);
-  }).catch((err) => {
-    throw err;
-  });
-}
+onload = async () => {
+  chrome.runtime.sendNativeMessage(
+    'native_messaging_local_server',
+    {},
+    async (nativeMessage) => {
+      parent.postMessage(nativeMessage, name);
+      await new Promise((resolve) => setTimeout(resolve, 20));
+      parent.postMessage('Ready', name);
+      onmessage = async (e) => {
+        const { value: file, done } = await e.data.getReader().read();
+        const fd = new FormData();
+        const stdin = await file.text();
+        fd.append(file.name, stdin);
+        const { body } = await fetch('http://localhost:8000', {
+          method: 'post',
+          body: fd,
+          cache: 'no-store',
+        });
+        parent.postMessage(body, name, [body]);
+        chrome.runtime.sendNativeMessage(
+          'native_messaging_local_server',
+          {},
+          (nativeMessage) => {
+            parent.postMessage(nativeMessage, name);
+          }
+        );
+      };
+    }
+  );
+};
